ea7f4b6175674d0e2eac581265f41e5c0d64825f
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2024-09-03 22:01:26 +0200

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -10,7 +10,7 @@ qt_x86_64_linux_packages := qt expat dbus libxcb xcb_proto libXau xproto freetyp
 qt_i686_linux_packages := $(qt_x86_64_linux_packages)
 
 # Definiere Qt-Pakete für ARM64 auf Linux
-qt_aarch64_linux_packages := qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 libXext xtrans
+qt_aarch64_linux_packages := qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 
 qt_darwin_packages = qt
 qt_mingw32_packages = qt
