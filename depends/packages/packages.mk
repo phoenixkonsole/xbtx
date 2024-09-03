c212327cef3bcdf9a68e0255d7c9b63cf9cbe04f
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2024-09-03 21:59:25 +0200

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,23 +1,26 @@
-packages:=boost openssl libevent zeromq
-native_packages := native_ccache native_b2
+packages := boost openssl libevent zeromq
+native_packages := native_ccache
 
+# Definiere Qt-Pakete, die sowohl für ARM64 als auch für x86 benötigt werden
 qt_native_packages = native_protobuf
-qt_packages = qrencode protobuf
+qt_packages = qrencode protobuf zlib
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon
-qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
-qt_arm_linux_packages:=$(qt_x86_64_linux_packages)
-qt_aarch64_linux_packages:=$(qt_x86_64_linux_packages)
+# Qt-Pakete für spezifische Architekturen und Betriebssysteme
+qt_x86_64_linux_packages := qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_i686_linux_packages := $(qt_x86_64_linux_packages)
 
-qt_darwin_packages=qt
-qt_mingw32_packages=qt
+# Definiere Qt-Pakete für ARM64 auf Linux
+qt_aarch64_linux_packages := qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 libXext xtrans
 
-wallet_packages=bdb
+qt_darwin_packages = qt
+qt_mingw32_packages = qt
 
-upnp_packages=miniupnpc
+wallet_packages = bdb
+
+upnp_packages = miniupnpc
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_libtapi native_cdrkit native_libdmg-hfsplus native_clang
+darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
 endif
