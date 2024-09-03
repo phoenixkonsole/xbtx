3b54355dc45ae4620e0c45a52466e1938848159e
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2024-09-03 22:16:37 +0200

    Update xextproto.mk

--- depends/packages/xextproto.mk
+++ depends/packages/xextproto.mk
@@ -5,7 +5,7 @@ $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc92aa5bb0
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared
+$(package)_config_opts=--disable-shared --build=$(BUILD) --host=$(HOST)
 endef
 
 define $(package)_config_cmds
