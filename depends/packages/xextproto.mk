3eb6fd7e546bb26bb729d8bf6e77746435138987
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2024-09-03 22:45:44 +0200

    Update xextproto.mk

--- depends/packages/xextproto.mk
+++ depends/packages/xextproto.mk
@@ -1,20 +1,19 @@
 package=xextproto
 $(package)_version=7.3.0
-$(package)_download_path=http://xorg.freedesktop.org/releases/individual/proto
+$(package)_download_path=https://xorg.freedesktop.org/releases/individual/proto
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc92aa5bb0
 
-define $(package)_set_vars
-$(package)_config_opts=--disable-shared --build=$(build_triplet) --host=$(host_triplet)
+define $(package)_preprocess_cmds
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
 endef
 
-define $(package)_preprocess_cmds
-  wget -O config.guess 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' && \
-  wget -O config.sub 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
+define $(package)_set_vars
+$(package)_config_opts=--disable-shared
 endef
 
 define $(package)_config_cmds
-  ./configure $($(package)_config_opts)
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
