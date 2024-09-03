5d245b1cef9af3abb905789bf45f7e276874c0d6
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2024-09-03 22:19:20 +0200

    Update xextproto.mk

--- depends/packages/xextproto.mk
+++ depends/packages/xextproto.mk
@@ -5,11 +5,16 @@ $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc92aa5bb0
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared --build=$(BUILD) --host=$(HOST)
+$(package)_config_opts=--disable-shared --build=$(build_triplet) --host=$(host_triplet)
+endef
+
+define $(package)_preprocess_cmds
+  wget -O config.guess 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' && \
+  wget -O config.sub 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  ./configure $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
