694f95b8fcbabb2b830bdd2c17f41553a5e76ce1
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2024-09-03 22:43:24 +0200

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,23 +1,20 @@
-packages := boost openssl libevent zeromq
-native_packages := native_ccache
+packages:=boost openssl libevent gmp
 
-# Definiere Qt-Pakete, die sowohl für ARM64 als auch für x86 benötigt werden
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-# Qt-Pakete für spezifische Architekturen und Betriebssysteme
-qt_x86_64_linux_packages := qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
-qt_i686_linux_packages := $(qt_x86_64_linux_packages)
+qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 
-# Definiere Qt-Pakete für ARM64 auf Linux
-qt_aarch64_linux_packages := qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_darwin_packages=qt
+qt_mingw32_packages=qt
 
-qt_darwin_packages = qt
-qt_mingw32_packages = qt
+wallet_packages=bdb curl
 
-wallet_packages = bdb
+zmq_packages=zeromq
 
-upnp_packages = miniupnpc
+upnp_packages=miniupnpc
+
+adns_packages=expat libunbound
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
